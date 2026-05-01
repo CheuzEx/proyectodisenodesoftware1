50d1ee5b00d60fda5ff06c2ea64b7b48ccf6a4e7
CheuzEx <114528843+CheuzEx@users.noreply.github.com>
2026-05-01 12:22:07 -0600

    Add files via upload

---
+++ clinica/clinica/proyectodisenodesoftware1-main/proyectodisenodesoftware1-main/clinica/src/main/java/edu/itcr/clinica/repository/PacienteRepository.java
@@ -0,0 +1,26 @@
+package edu.itcr.clinica.repository;
+
+import edu.itcr.clinica.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    //  para obtener todos los pacientes ordenados por apellido y nombre
+    List<Paciente> findAllByOrderByApellidoAscNombreAsc();
+
+    // Busca pacientes por nombre o apellido (case-insensitive) y ordenar
+    @Query("""
+        SELECT p FROM Paciente p
+        WHERE LOWER(p.nombre) LIKE LOWER(CONCAT('%', :q, '%'))
+           OR LOWER(p.apellido) LIKE LOWER(CONCAT('%', :q, '%'))
+        ORDER BY p.apellido ASC, p.nombre ASC
+    """)
+    List<Paciente> searchByNombreOrApellido(@Param("q") String q);
+}
+
